06df8100e6eb857c8ea458ad87e5528fc419e272
Linuss <35455460+rainthart@users.noreply.github.com>
2022-10-07 14:12:19 +0200

    JavaScript simplified!

--- index.js
+++ index.js
@@ -1,24 +1,3 @@
-function navLinkScroll() {
-  let liLength = document.getElementById("ul_o").getElementsByTagName("li");
-
-  console.log(liLength);
-  console.log(liLength.length);
-
-  for (let i = 0; i < liLength.length; i++) {
-    document.getElementsByTagName("h2")[i].scrollIntoView();
-    console.log(document.getElementsByTagName("h2")[i]);
-  }
-}
-
-function navLinkScrollFirst() {
-  console.log(document.getElementsByTagName("h2")[0].scrollIntoView());
-  document.getElementsByTagName("h2")[0].scrollIntoView();
-}
-
-function navLinkScrollSecond() {
-  document.getElementsByTagName("h2")[1].scrollIntoView();
-}
-
-function navLinkScrollThird() {
-  document.getElementsByTagName("h2")[2].scrollIntoView();
+function navLinkScroll(x) {
+  document.getElementsByTagName("h2")[x].scrollIntoView();
 }
